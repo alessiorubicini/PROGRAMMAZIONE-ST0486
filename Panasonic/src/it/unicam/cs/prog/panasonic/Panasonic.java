cf583ba84c7a1b3b7b72bd79d7560983f24cb001
Alessio Rubicini <alessiorubicini05@gmail.com>
2022-01-10 17:43:27 +0100

    Correzioni varie

--- Panasonic/src/it/unicam/cs/prog/panasonic/Panasonic.java
+++ Panasonic/src/it/unicam/cs/prog/panasonic/Panasonic.java
@@ -1,79 +1,55 @@
 package it.unicam.cs.prog.panasonic;
 
-import java.util.ArrayList;
-
 public class Panasonic extends TV {
-	private int numeroDiCanali;
 	private String risoluzione;
-	private ArrayList<Canale> canali;
+	private int numeroDiCanali;
+	private int[] canali;
 	
 	/**
 	 * Crea un oggetto Panasonic
 	 * @param modello: nome del modello
 	 * @param pollici: numero di pollici dello schermo
-	 * @param numeroDiCanali: numero di canali, strettamente maggiore di 12
+	 * @param numeroDiCanali: numero di canali, strettamente minore di 12
 	 * @param risoluzione: risoluzione dello schermo (es. Full-HD)
 	 * @throws Exception se il numero di canali inseriti è minore di 12
 	 */
 	public Panasonic(String modello, int pollici, int numeroDiCanali, String risoluzione) throws Exception {
 		super(pollici);
-		if(numeroDiCanali > 12) {
+		if(numeroDiCanali < 13) {
 			this.nomeModello = modello;
 			this.numeroDiCanali = numeroDiCanali;
 			this.risoluzione = risoluzione;
-			this.canali = new ArrayList<Canale>();
+			this.canali = new int[numeroDiCanali];
 		} else {
 			throw new Exception("Numero di canali errato");
 		}
 	}
 
-	public int getNumeroDiCanali() { return numeroDiCanali; }
-
-	public void setNumeroDiCanali(int numeroDiCanali) { this.numeroDiCanali = numeroDiCanali; }
-
 	public String getRisoluzione() { return risoluzione; }
-
-	public void setRisoluzione(String risoluzione) { this.risoluzione = risoluzione; }
-	
-	/**
-	 * Controlla se la frequenza è impostata per altri canali o meno
-	 * @param frequenza in Mhz
-	 * @return true se la frequenza è libera, false se è già presa da un altro canalae
-	 */
-	private boolean frequenzaLibera(int frequenza) {
-		for(Canale canale: this.canali) {
-			if(canale.getFrequenza() == frequenza) {
-				return false;
-			}
-		}
-		return true;
-	}
 	
 	/**
 	 * Imposta un nuovo canale sulla televisione
-	 * @param numero del canale
-	 * @param frequenza del canale in Mhz
+	 * @param numero del canale, compreso tra 1 e il numero di canali della TV
+	 * @param frequenza del canale in Mhz, compresa tra 550 e 790 MHz
 	 * @throws CanaliSovrappostiException se la frequenza è già presa da un altro canale
 	 */
 	public void setCanale(int numero, int frequenza) throws CanaliSovrappostiException {
-		if(this.frequenzaLibera(frequenza)) {
-			try {
-				this.canali.add(new Canale(numero, frequenza));
-				this.numeroDiCanali++;
-			} catch (Exception e) {
-				System.out.println(e);
+		if((numero >= 1 && numero <= this.numeroDiCanali) && (frequenza >= 550 && frequenza <= 790)) {
+			for(int i = 0; i < canali.length; i++) {
+				if(this.canali[i] == frequenza) {
+					throw new CanaliSovrappostiException();
+				}
 			}
-		} else {
-			throw new CanaliSovrappostiException();
+			this.canali[numero - 1] = frequenza;
 		}
 	}
 	
 	@Override
 	public String toString() {
 		String risultato = "";
-		risultato += "Modello: " + this.nomeModello + ", pollici: " + this.getPollici() + ", risoluzione: " + this.risoluzione + ". Canali:\n";
-		for(Canale canale: this.canali) {
-			risultato += "- Canale " + canale.getNumero() + ", frequenza: " + canale.getFrequenza() + "Mhz\n";
+		risultato += "Modello: " + this.nomeModello + ", pollici: " + this.getPollici() + ", risoluzione: " + this.risoluzione + "\n";
+		for(int i = 0; i < canali.length; i++) {
+			risultato += "- Canale " + (i+1) + " " + this.canali[i] + "Mhz\n";
 		}
 		return risultato;
 	}
@@ -82,13 +58,14 @@ public class Panasonic extends TV {
 	public static void main(String[] args) {
 		Panasonic tv;
 		try {
-			tv = new Panasonic("SmartTV 2021", 50, 20, "Full-HD");
+			tv = new Panasonic("SmartTV 2021", 50, 10, "Full-HD");
 		} catch (Exception e) {
 			System.out.println(e);
 			return;
 		}
 		try {
-			tv.setCanale(8, 700);
+			tv.setCanale(1, 700);
+			tv.setCanale(2, 600);
 		} catch (CanaliSovrappostiException e) {
 			System.out.println(e);
 		}
